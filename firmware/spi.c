539e22ec8f1f19b9954ac8204a77168c0d868283
Martin Sivak <msivak@MaMac.local>
2012-04-13 14:44:19 +0200

    change SPI settings

--- firmware/spi.c
+++ firmware/spi.c
@@ -5,8 +5,8 @@ void spi_init(void)
     SPI_DDR |= _BV(SPI_SCK) | _BV(SPI_MOSI) | _BV(SPI_SS);
     SPI_PORT |= _BV(SPI_SS);
 
-    // Enable SPI, Master, set clock rate fck/4, CLK idle high
-    SPCR = _BV(SPE) | _BV(MSTR) | _BV(CPOL) | _BV(CPHA);
+    // Enable SPI, Master, set clock rate fck/4, CLK idle low, sample at H->L
+    SPCR = _BV(SPE) | _BV(MSTR) | _BV(CPHA);
     // SPSR |= _BV(SPI2X);
 }
 
